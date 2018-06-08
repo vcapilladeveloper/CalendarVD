3117efe0b89129cb52e58f3bffbb66bff372fe5d
Victor Capilla Borrego <victor.capilla@gmail.com>
2018-06-08 20:27:33 +0200

    Initial Commit

---
+++ CalendarVD/CalendarVD.h
@@ -0,0 +1,19 @@
+//
+//  CalendarVD.h
+//  CalendarVD
+//
+//  Created by Victor Capilla Borrego on 8/6/18.
+//  Copyright © 2018 Victor Capilla Borrego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarVD.
+FOUNDATION_EXPORT double CalendarVDVersionNumber;
+
+//! Project version string for CalendarVD.
+FOUNDATION_EXPORT const unsigned char CalendarVDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarVD/PublicHeader.h>
+
+
